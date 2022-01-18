b80bb9d9c34eeb8d9571087b2b08ad417edf3f67
Vichitra Kumar Ettiyadathu Velayudhan <vichitrakumar1982@gmail.com>
2022-01-18 07:37:14 -0600

    Adding some comments for better understanding of the code

--- Program.cs
+++ Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RetailerProgram
 {
@@ -16,7 +12,8 @@ namespace RetailerProgram
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                string sqlCommandText = @"--Given a date criteria we will work backwards
+                string sqlCommandText = @"
+--Given a date criteria we will work backwards
 DECLARE @date DATE = '3/20/2021';
 
 --using a recurssive Common Table expression to create the date ranges for the past 3 months
@@ -27,7 +24,7 @@ WITH MonthsCTE AS (
     SELECT [Month] + 1, DATEADD(MONTH, -1, MonthStart) as 'MonthStart', EOMonth(DATEADD(MONTH, -1, MonthStart)) as 'MonthEnd'
     FROM MonthsCTE 
     WHERE [Month] < 3 ),
---customerdata holds the data from Customers, Orders and OrderDetails
+--customerdata holds the data from Customers and Orders
 --based on the list of start and end ranges available
 CustomerData as (
     SELECT  cus.CustomerId, 
@@ -40,21 +37,10 @@ CustomerData as (
                 ELSE 0
             END +
             CASE WHEN (ord.TotalPrice - 50) > 0 
-                --THEN CAST(FLOOR((ord.TotalPrice - 50) / 50) as float) * 50
                 THEN 50
                 ELSE 0
             END)
             as Rewards
-            --SUM(CASE WHEN (ord.TotalPrice - 100) > 0 
-            --    THEN ((ord.TotalPrice - 100) * 2) 
-            --    ELSE 0
-            --END) as RewardsLHS,
-            --SUM(CASE WHEN (ord.TotalPrice - 50) > 0 
-            --    THEN CAST(FLOOR((ord.TotalPrice - 50) / 50) as float) * 50
-            --    ELSE 0
-            --END) as RewardsRHS
-            
-    --FROM MonthsCTE cte,
     FROM    Customers cus,
 	        Orders ord
     where cus.CustomerId = ord.CustomerId
@@ -67,8 +53,6 @@ SELECT  cus.CustomerId,
         cte.MonthEnd, 
         Sum(ord.TotalPrice) as Total, 
         Sum(Rewards) as TotalRewards
-        --Sum(RewardsLHS) as TotalRewardsLHS,
-        --Sum(RewardsRHS) as TotalRewardsRHS
 FROM    CustomerData cus,
         Orders ord,
         MonthsCTE cte
@@ -76,10 +60,12 @@ where   cus.OrderDate between cte.MonthStart and cte.MonthEnd
 and     ord.CustomerId = cus.CustomerId
 and     ord.OrderDate = cus.OrderDate
 Group by cus.CustomerId, cus.CustomerName, cus.ContactName, cte.MonthStart, cte.MonthEnd;";
+                //Creating a sql command to execute the above query
                 using (SqlCommand command = new SqlCommand(sqlCommandText, connection))
                 {
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
+                        //Displaying the headers for customers record
                         for (int i = 0; i < reader.FieldCount; i++)
                         {
                             if (i == 0)
@@ -88,7 +74,7 @@ Group by cus.CustomerId, cus.CustomerName, cus.ContactName, cte.MonthStart, cte.
                                 Console.Write(reader.GetName(i) + "\t");
                         }
                         Console.WriteLine("\n\t_____________________________________________________________________________________________________\n");
-                        //Console.WriteLine();
+                        //Displaying the records for each customer for a month's timeframe
                         while (reader.Read())
                         {
                             for (int i = 0; i < reader.FieldCount; i++)
@@ -102,7 +88,11 @@ Group by cus.CustomerId, cus.CustomerName, cus.ContactName, cte.MonthStart, cte.
                             int customerId = reader.GetInt32(0);
                             DateTime startDate = reader.GetDateTime(3);
                             DateTime endDate = reader.GetDateTime(4);
-                            string sqlText = @"select OrderDate, TotalPrice, (CASE WHEN (TotalPrice - 100) > 0 
+                            //Creating a sub-query to loop through each transactions
+                            //Assuming there will be an API which will create the json for us in an hierarchical manner.
+                            //Because of time crunch I have handled it this way
+                            string sqlText = @"
+                                select OrderDate, TotalPrice, (CASE WHEN (TotalPrice - 100) > 0 
                                     THEN ((TotalPrice - 100) * 2) 
                                     ELSE 0
                                 END +
@@ -113,6 +103,7 @@ Group by cus.CustomerId, cus.CustomerName, cus.ContactName, cte.MonthStart, cte.
                                 as Rewards from Orders
                                 where CustomerId = @customerId
                                 and OrderDate between @startDate and @endDate";
+                            //Executing the above query for each customer for the given date range (a month)
                             using (SqlCommand sqlCommand = new SqlCommand(sqlText, connection))
                             {
                                 //sqlCommand.Parameters.Clear();
@@ -122,6 +113,7 @@ Group by cus.CustomerId, cus.CustomerName, cus.ContactName, cte.MonthStart, cte.
                                 using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
                                 {
                                     Console.Write("\t\t\t");
+                                    //Displaying the header table for each order details for a given transaction
                                     for (int i = 0; i < dataReader.FieldCount; i++)
                                     {
                                         if (i == 0)
@@ -130,6 +122,7 @@ Group by cus.CustomerId, cus.CustomerName, cus.ContactName, cte.MonthStart, cte.
                                             Console.Write(dataReader.GetName(i) + "\t");
                                     }
                                     Console.WriteLine("\n\t_____________________________________________________________________________________________________\n");
+                                    //Displaying the rows for each order details for a given transaction
                                     while (dataReader.Read())
                                     {
                                         Console.Write("\t\t\t");
@@ -149,7 +142,6 @@ Group by cus.CustomerId, cus.CustomerName, cus.ContactName, cte.MonthStart, cte.
                     }
                 }
             }
-            //Console.WriteLine("Connection string from app.config is " + ConfigurationManager.ConnectionStrings["RetailerRewardsContext"].ConnectionString);
             Console.Read();
         }
     }
